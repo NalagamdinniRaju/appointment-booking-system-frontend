bdc1ed928949dc17fc76b4bfd2f8ba914d8c9682
NalagamdinniRaju <nalagamdinniraju@gmail.com>
2025-02-26 10:16:59 +0530

    first commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,30 @@
+
+// Navbar.js
+import { Link } from "react-router-dom"
+import { FaCalendarAlt, FaUserMd } from "react-icons/fa"
+import "./Navbar.css"
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <div className="navbar-container">
+        <Link to="/" className="logo">
+          BabySteps
+        </Link>
+        <div className="navbar-links">
+
+          <Link to="/" className="nav-link">
+  <FaUserMd className="link-icon" />
+  <span>Doctors</span>
+</Link>
+<Link to="/appointments" className="nav-link">
+  <FaCalendarAlt className="link-icon" />
+  <span>My Appointments</span>
+</Link>
+        </div>
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar
